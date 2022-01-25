28c3d52e43f297e223dfd0eea233f426cb4ae3d0
phil294 <philip@waritschlager.de>
2022-01-25 11:26:54 +0100

    fix packaging process
    coffeescript dev package in node_modules from github contained 1. unnecessary bloat and 2. broken links inside docs folder that even failed the build

--- release.sh
+++ release.sh
@@ -11,6 +11,8 @@ npm publish
 cd ..
 yarn compile
 yarn prepare-publish 
+rm -rf server/node_modules/coffeescript/{docs,documentation,.github,test,src}
+rm -rf server/node_modules/coffeescript/lib/{coffeescript,coffeescript-browser-compiler-legacy}
 vsce package
 vsce publish
 git push origin master
