1f8c8e657c5be3f05a880c42f4fc0b5531222668
phil294 <philip@waritschlager.de>
2022-02-25 03:40:21 +0100

    add "Show JS" option to doctor popup, which opens a new editor with the js contents

--- client/commands/doctorCommand.ts
+++ client/commands/doctorCommand.ts
@@ -11,9 +11,14 @@ export function generateDoctorCommand(client: LanguageClient) {
 
     const result = (await client.sendRequest('$/doctor', { fileName: active_filename })) as string;
     const showText = result.slice(0, 1000) + '....';
-    const action = await vscode.window.showInformationMessage(showText, { modal: true }, 'Ok', 'Copy');
+    const action = await vscode.window.showInformationMessage(showText, { modal: true }, 'Ok', 'Copy', 'Show JS');
     if (action === 'Copy') {
       await vscode.env.clipboard.writeText(result);
+    } else if (action === 'Show JS') {
+      const new_doc = await vscode.workspace.openTextDocument({ language: 'javascript' })
+      const new_editor = await vscode.window.showTextDocument(new_doc)
+      const js = JSON.parse(result).js
+      await new_editor.edit(t => t.insert(new vscode.Position(0, 0), js), { undoStopBefore: true, undoStopAfter: false })
     }
   };
 }
