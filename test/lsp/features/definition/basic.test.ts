297a5f7781e7304610b89e53218a52637fde934d
phil294 <philip@waritschlager.de>
2022-02-28 09:12:11 +0100

    add disabled comprehension test

--- test/lsp/features/definition/basic.test.ts
+++ test/lsp/features/definition/basic.test.ts
@@ -16,6 +16,15 @@ describe('Should find definition', () => {
 		await testDefinition(basic_uri, position(0, 12), sameLineLocation(lodashDtsUri, 246, 12, 13))
 	})
 
+	// comprehensions introduce coffee code that is absent in js, and as such cannot be reverse mapped as is
+	xit('finds definition in comprehension', async () => {
+		await testDefinition(basic_uri, position(11, 37), sameLineLocation(basic_uri, 10, 0, 15))
+	})
+
+	xit('finds definition in comprehension when variable is not a simple assignment', async () => {
+		await testDefinition(basic_uri, position(14, 37), sameLineLocation(basic_uri, 13, 0, 15))
+	})
+
 	// TODO: Currently impossible, looks like CS source maps bug, and is same bug without the `if`
 	xit('completes an if-statement with optional chaining and sub properties', async () => {
 		await testDefinition(chained_if_uri, position(1, 20), sameLineLocation(chained_if_uri, 0, 13, 29))
